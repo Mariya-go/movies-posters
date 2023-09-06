2ac08330524f5d9d8e21ca349513485b29f1eef4
Mariya-go <mariya.golovko@gmail.com>
2023-09-06 15:51:35 +0200

    update url

--- src/data.js
+++ src/data.js
@@ -1,6 +1,7 @@
 const data = {
-    baseUrl: 'http://localhost:5000/movies'
+    // baseUrl: 'http://localhost:5000/movies'
     // baseUrl: 'https://my-json-server.typicode.com/Mariya-go/movies-posters/movies'
+    baseUrl: 'https://my-json-server.typicode.com/samirm00/movies-list/movies'
 };
 export default data;
 //  https://my-json-server.typicode.com/samirm00/movies-list/movies
